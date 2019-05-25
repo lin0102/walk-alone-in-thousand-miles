bb19fb24695de85bf5ee68054880757bac249c4a
lin0102 <1262280723@qq.com>
2019-05-25 23:52:24 +0800

    daily update

--- src/script/GameUI.ts
+++ src/script/GameUI.ts
@@ -1,51 +1,58 @@
-import { ui } from "./../ui/layaMaxUI";
-import CameraMove from './CameraMove';
+import { ui } from "./../ui/layaMaxUI"
+import CameraMove from './CameraMove'
 import marshalMove from './marshalMove'
+import position from './position'
 
 export default class GameUI extends ui.test.TestSceneUI {
+    public Scene: Laya.Scene3D
+    public MAXNUMOFSCENE: number = 1000
+    public i: number = 0
+    public cloneScenes: Laya.MeshSprite3D[] = []
     constructor() {
-        super();
-
-        const MAXNUMOFSCENE = 1000;
-
-        Laya.Scene3D.load("res/marshal.ls", Laya.Handler.create(null, function (s: Laya.Scene3D): void {
-            let scene: Laya.Scene3D = Laya.stage.addChild(s) as Laya.Scene3D;
-
-            scene.enableFog = true;
-			scene.fogStart = 1;
-			scene.fogRange = 10;
-
-            let sceneMesh: Laya.MeshSprite3D = scene.getChildByName('cjMerge') as Laya.MeshSprite3D;
-
-            let cloneScenes:Laya.MeshSprite3D[] = [];
-            for(let i = 0; i < MAXNUMOFSCENE; i++) {
-                let cloneScene: Laya.MeshSprite3D = sceneMesh.clone();
-                cloneScenes.push(cloneScene);
-            }
-
-            let i = 0;
-            while(i < 50) {
-                scene.addChild(cloneScenes[i++]);
-                cloneScenes[i].transform.translate(new Laya.Vector3(0, 0, -3.54 * i), false);
-            }
-
-            function loadMoreScene() {
-                scene.addChild(cloneScenes[i++]);
-                cloneScenes[i].transform.translate(new Laya.Vector3(0, 0, -3.54 * i), false);
-                if(i === MAXNUMOFSCENE) {
-                    Laya.timer.clear(this, loadMoreScene);
-                }
-            }
-            Laya.timer.loop(2000, this, loadMoreScene);
-
-            let camera: Laya.Camera = scene.getChildByName('Camera') as Laya.Camera;
-            camera.addComponent(CameraMove);
-
-            let marshal: Laya.Sprite3D = scene.getChildByName('jiang') as Laya.Sprite3D;
-            marshal.addComponent(marshalMove);
-        }));
+        super()
+        this.loadScene()
     }
-    test() {
-        
+    public loadScene(): void {
+        Laya.Scene3D.load("res/marshal.ls", Laya.Handler.create(this, function (s: Laya.Scene3D): void {
+            this.Scene = Laya.stage.addChild(s)
+            this.Scene.enableFog = true
+            this.Scene.fogStart = 1
+            this.Scene.fogRange = 10
+
+            let camera: Laya.Camera = this.Scene.getChildByName('Camera') as Laya.Camera
+            camera.addComponent(CameraMove)
+
+            // let marshal: Laya.Sprite3D = this.Scene.getChildByName('jiang') as Laya.Sprite3D
+            // marshal.addComponent(marshalMove)
+
+            // let enemySprite = {
+            //     shi: this.Scene.getChildByName('shi'),
+            //     ju: this.Scene.getChildByName('ju'),
+
+            // }
+
+            let sceneMesh: Laya.MeshSprite3D = this.Scene.getChildByName('cjMerge') as Laya.MeshSprite3D
+            this.loadMoreScene(sceneMesh)
+        }))
+    }
+    public loadMoreScene(sceneMesh: Laya.MeshSprite3D): void {
+        for (let i = 0; i < this.MAXNUMOFSCENE; i++) {
+            let cloneScene: Laya.MeshSprite3D = sceneMesh.clone()
+            this.cloneScenes.push(cloneScene)
+        }
+
+        while (this.i < 50) {
+            this.Scene.addChild(this.cloneScenes[this.i])
+            this.cloneScenes[this.i].transform.translate(new Laya.Vector3(0, 0, -3.54 * ++this.i), false)
+        }
+
+        Laya.timer.loop(2000, this, this.addMoreScene)
+    }
+    public addMoreScene(): void {
+        this.Scene.addChild(this.cloneScenes[this.i])
+        this.cloneScenes[this.i].transform.translate(new Laya.Vector3(0, 0, -3.54 * ++this.i), false)
+        if (this.i === this.MAXNUMOFSCENE) {
+            Laya.timer.clear(this, this.addMoreScene)
+        }
     }
 }
---
+++ src/script/position.ts
@@ -0,0 +1,30 @@
+enum enemy {xiang = 'xiang', shi = 'shi', ju = 'ju', pao = 'pao', ma = 'ma', zu = 'zu'}
+
+const position = [
+    {
+        deadPosition: [
+            {
+                x: 3,
+                y: 2,
+            },
+            {
+                x: 3,
+                y: 2,
+            }
+        ],
+        enemyPosition: [
+            {
+                name: enemy.xiang,
+                x: 0,
+                y: 2
+            },
+            {
+                name: enemy.xiang,
+                x: 0,
+                y: 2
+            }
+        ]
+    }   
+]
+
+export default position
