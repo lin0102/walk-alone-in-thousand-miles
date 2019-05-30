8e02d3e3793714e86fa315eac9a1746873f7a6c4
lin0102 <1262280723@qq.com>
2019-05-30 23:31:24 +0800

    改bug

--- src/script/position.ts
+++ src/script/position.ts
@@ -3,6 +3,312 @@ enum enemy {xiang = 'xiang', shi = 'shi', ju = 'ju', pao = 'pao', ma = 'ma', zu 
 const position = [
     {
         deadPosition: [
+            
+        ],
+        enemyPosition: [
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 1,
+                y: 3,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.shi,
+                x: 0,
+                y: 4
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+        ],
+        enemyPosition: [
+            {
+                name: enemy.pao,
+                x: 1,
+                y: 4
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 1,
+                y: 3,
+            },
+            {
+                x: 0,
+                y: 4,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.ma,
+                x: 2,
+                y: 5
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 1,
+                y: 2,
+            },
+            {
+                x: 2,
+                y: 3,
+            }
+        ],
+        enemyPosition: [
+            {
+                name: enemy.ma,
+                x: 0,
+                y: 4
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 0,
+                y: 3,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.zu,
+                x: 0,
+                y: 4
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 2,
+                y: 3,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.xiang,
+                x: 0,
+                y: 5
+            }
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 2,
+                y: 2,
+            },
+            {
+                x: 0,
+                y: 2,
+            }
+        ],
+        enemyPosition: [
+            {
+                name: enemy.ma,
+                x: 1,
+                y: 4
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 0,
+                y: 0,
+            },
+            {
+                x: 0,
+                y: 1,
+            },
+            {
+                x: 0,
+                y: 2,
+            },
+            {
+                x: 0,
+                y: 3,
+            }
+        ],
+        enemyPosition: [
+            {
+                name: enemy.pao,
+                x: 0,
+                y: 5
+            },
+            {
+                name: enemy.zu,
+                x: 0,
+                y: 4
+            }
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 1,
+                y: 3,
+            },
+            {
+                x: 2,
+                y: 3,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.shi,
+                x: 0,
+                y: 4
+            },
+            {
+                name: enemy.xiang,
+                x: 0,
+                y: 5
+            }
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 0,
+                y: 3,
+            },
+            {
+                x: 2,
+                y: 3,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.ma,
+                x: 1,
+                y: 5
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 1,
+                y: 5,
+            },
+            {
+                x: 2,
+                y: 5,
+            },
+            {
+                x: 2,
+                y: 4,
+            },
+            {
+                x: 2,
+                y: 3,
+            },
+            {
+                x: 2,
+                y: 2
+            }
+        ],
+        enemyPosition: [
+            {
+                name: enemy.zu,
+                x: 1,
+                y: 6
+            },
+            {
+                name: enemy.ju,
+                x: 2,
+                y: 6
+            }
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 2,
+                y: 3,
+            },
+            {
+                x: 1,
+                y: 2,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.ma,
+                x: 0,
+                y: 4
+            },
+        ]
+    }, 
+    {
+        deadPosition: [
+            {
+                x: 0,
+                y: 4,
+            },
+            {
+                x: 2,
+                y: 2,
+            }
+        ],
+        enemyPosition: [
+            {
+                name: enemy.zu,
+                x: 0,
+                y: 5
+            },
+            {
+                name: enemy.xiang,
+                x: 0,
+                y: 4
+            }
+        ]
+    },
+    {
+        deadPosition: [
+            {
+                x: 1,
+                y: 2,
+            },
+            {
+                x: 2,
+                y: 3,
+            },
+        ],
+        enemyPosition: [
+            {
+                name: enemy.shi,
+                x: 2,
+                y: 3
+            },
+            {
+                name: enemy.xiang,
+                x: 0,
+                y: 5
+            }
+        ]
+    }, 
+
+
+
+
+
+
+
+    
+    {
+        deadPosition: [
         ],
         enemyPosition: [
         ]
